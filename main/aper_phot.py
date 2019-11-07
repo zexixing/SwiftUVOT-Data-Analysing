069edce9be04e766c7279066e81630e68aa1d773
Xing Zexi <zexixing0604@gmail.com>
2019-11-06 22:47:21 -0600

    plot to get optimum regions for the photometry

--- main/aper_phot.py
+++ main/aper_phot.py
@@ -1,21 +1,76 @@
 import numpy as np
 from astropy.io import fits
 from tools import get_path
-from scipy import interpolate
 
-def filt_para(filt):
-    para = {'v': {'fwhm': 769, 'zp': 17.89, 'cf': 2.61e-16},
-            'b': {'fwhm': 975, 'zp': 19.11, 'cf': 1.32e-16},
-            'u': {'fwhm': 785, 'zp': 18.34, 'cf': 1.5e-16},
-            'uw1': {'fwhm': 693, 'zp': 17.49, 'cf': 4.3e-16, 'rf': 0.138},
-            'um2': {'fwhm': 498, 'zp': 16.82, 'cf': 7.5e-16},
-            'uw2': {'fwhm': 657, 'zp': 17.35, 'cf': 6.0e-16}
-            }
-    return para[filt]
+def load_img(img_name):
+    img_path = get_path('../docs/'+img_name)
+    img_data = fits.open(img_path)[0].data
+    return img_data
+
+def load_header(img_name):
+    img_path = get_path('../docs/'+img_name)
+    img_header = fits.open(img_path)[0].header
+    return img_header
+
+def get_dist(point_1, point_2):
+    i2 = (point_1[0]-point_2[0])**2
+    j2 = (point_1[1]-point_2[1])**2
+    dist = np.sqrt(i2+j2)
+    return dist
+
+def limit_loop(img_name, center, r):
+    img_data = load_img(img_name)
+    i_range = np.arange(len(img_data))
+    j_range = np.arange(len(img_data[0]))
+    cen_pix = [center[1]-1, center[0]-1]
+    i_range = i_range[i_range > cen_pix[0]-r]
+    i_range = i_range[i_range < cen_pix[0]+r]
+    j_range = j_range[j_range > cen_pix[1]-r]
+    j_range = j_range[j_range < cen_pix[1]+r]
+    return img_data, cen_pix, i_range, j_range
+
+def circle_ct(img_name, center, r):
+    img_data, cen_pix, i_range, j_range = \
+        limit_loop(img_name, center, r)
+    pixel = 0
+    count = 0
+    for i in i_range:
+        for j in j_range:
+            pos_pix = [i, j]
+            pos_cen_dist = get_dist(cen_pix, pos_pix)
+            if pos_cen_dist < r:
+                pixel += 1
+                count += img_data[i, j]
+    return count, pixel
+
+def donut_ct(img_name, center, r):
+    r1 = r[0]
+    r2 = r[1]
+    img_data, cen_pix, i_range, j_range = \
+        limit_loop(img_name, center, r2)
+    pixel = 0
+    count = 0
+    for i in i_range:
+        for j in j_range:
+            pos_pix = [i, j]
+            pos_cen_dist = get_dist(cen_pix, pos_pix)
+            if pos_cen_dist > r1 and pos_cen_dist < r2:
+                pixel += 1
+                count += img_data[i, j]
+    return count, pixel
+
+def multi_circle_ct(img_name, center_list, r_list):
+    count_list = []
+    pixel_list = []
+    for n in range(len(r_list)):
+        count, pixel = circle_ct(img_name, center_list[n], r_list[n])
+        count_list.append(count)
+        pixel_list.append(pixel)
+    return count_list, pixel_list
 
 def aper_phot(src_count, src_pixel, 
-               bg_bri, exposure,
-               src_count_err, bg_bri_err):
+              bg_bri, exposure, 
+              src_count_err, bg_bri_err):
     """aperture photometry
     """
     bg_count = bg_bri*src_pixel
@@ -28,85 +83,37 @@ def aper_phot(src_count, src_pixel,
     snr = net_cr/net_cr_err
     return net_cr, net_cr_err, snr
 
-def cr2mag(cr, cr_err, filt):
-    """use zero points to transfer counts to mag
-    """
-    zp = filt_para(filt)['zp']
-    mag = zp - 2.5*np.log10(cr)
-    if cr_err == False:
-        return mag
+def load_reg_list(reg_name):
+    reg_path = get_path('../docs/'+reg_name)
+    reg_file = open(reg_path)
+    center_i_list = []
+    center_j_list = []
+    radiu_list = []
+    for line in islice(reg_file, 3, None):
+        reg_data = line.split('(')[1]
+        reg_data = reg_data.split(')')[0]
+        reg_data = reg_data.split[',']
+        center_j_list.append(reg_data[0])
+        center_i_list.append(reg_data[1])
+        radiu_list.append(reg_data[2:])
+    return center_i_list, center_j_list, radiu_list
+
+def reg2bg_bri(img_name, bg_method, bg_center, bg_r, n=3):
+    if bg_method == 'single':
+        bg_count, bg_pixel = circle_ct(img_name, bg_center, bg_r)
+    elif bg_method == 'donut':
+        bg_count, bg_pixel = dount_ct(img_name, bg_center, bg_r)
+    elif bg_method == 'multi':
+        bg_count, bg_pixel = multi_circle_ct(img_name, bg_center, bg_r)
+        bg_count = np.array(bg_count)
+        bg_pixel = np.array(bg_pixel)
     else:
-        mag_err = 2.5*cr_err/(np.log(10)*cr)
-        return mag, mag_err
-
-def cr2mean_flux(cr, cr_err, filt):
-    # read ea
-    ea_path = get_path('../data/auxil/arf_'+filt+'.fits')
-    ea_data = fits.open(ea_path)[1].data
-    ea_wave = (ea_data['WAVE_MIN']+ea_data['WAVE_MAX'])/20. # A to nm
-    ea_area = ea_data['SPECRESP']
-    delta_wave = ea_wave[1] - ea_wave[0]
-    factor = 0
-    for i in range(len(ea_wave)):
-        factor += ea_area[i]*ea_wave[i]*delta_wave*1e8*5.034116651114543
-    mean_flux = cr/(factor*10) # 10: nm to A
-    mean_flux_err = cr_err/(factor*10)
-    snr = mean_flux/mean_flux_err
-    return mean_flux, mean_flux_err, snr
-
-def cr2sb(cr, cr_err, filt, solid_angle):
-    """convert count rate to surface brightness
-    unit: W m-2 sr-1
-
-    cr to flux density: Poole et al. 2008
-    flux den to flux: flux den*FWHM
-    flux to sf: flux*factors/solid_angle
-    factors: 1 arcsec2 = 2.35e-11 sr
-             1 erg s-1 = 1e-7W
-             1 cm2 = 1e-4m2
-    """
-    fwhm = filt_para(filt)['fwhm']
-    cf = filt_para(filt)['cf']
-    factors = 1. #TODO:
-    sb = cr*cf*fwhm*factors/solid_angle
-    if cr_err == False: 
-        return sb
+        raise Exception('check the input of method')
+    if bg_method == 'multi':
+        bg_bri = bg_count/bg_pixel
+        bg_bri = np.mean(bg_bri)
+        bg_bri_err = (1/n)*np.sqrt(np.sum(bg_count/np.power(bg_pixel, 2)))
     else:
-        sb_err = cr_err*cf*fwhm*factors/solid_angle
-        return sb, sb_err
-
-def mag_sb_flux_from_spec(spec_name, filt):
-    """use effective area and theoretical spectra
-    to calculate apparent magnitude
-    """
-    # read spectra
-    spec_path = get_path('../data/auxil/'+spec_name)
-    spec_wave = np.loadtxt(spec_path)[:, 0]
-    spec_flux = np.loadtxt(spec_path)[:, 1]*2.720E-4 # flux moment to irradiance
-    # read ea
-    ea_path = get_path('../data/auxil/arf_'+filt+'.fits')
-    ea_data = fits.open(ea_path)[1].data
-    ea_wave = (ea_data['WAVE_MIN']+ea_data['WAVE_MAX'])/20. # A to nm
-    ea_area = ea_data['SPECRESP']
-    # interpolate ea to cater for spec
-    ea = interpolate.interp1d(ea_wave, ea_area, fill_value='extrapolate')
-    spec_ea = ea(spec_wave)
-    wave_min = max([np.min(spec_wave), np.min(ea_wave)])
-    wave_max = min([np.max(spec_wave), np.max(ea_wave)])
-    spec = np.c_[np.c_[spec_wave, spec_flux.T], spec_ea.T]
-    spec_reduce = spec[spec[:,0]>wave_min, :]
-    spec_reduce = spec_reduce[spec_reduce[:,0]<wave_max, :]
-    spec = spec_reduce[spec_reduce[:,2]>0, :]
-    # integral
-    delta_wave = spec[1, 0] - spec[0, 0]
-    cr = 0
-    flux = 0
-    A = 0 # 30 
-    for i in range(len(spec)):
-        cr += spec[i, 0]*spec[i, 1]*spec[i, 2]*delta_wave*1e8*5.034116651114543
-        flux += spec[i, 1]*spec[i, 2]*delta_wave
-        A += spec[i, 2]*delta_wave
-    flux = flux/A
-    flux = flux/10. # nm to A
-    # cr to mag
-    return cr2mag(cr, False, filt), cr2sb(cr, False, filt, 1.), flux  
+        bg_bri = bg_count/bg_pixel
+        bg_bri_err = np.sqrt(bg_count)/bg_pixel
+    return bg_bri, bg_bri_err
---
+++ main/cr2others.py
@@ -0,0 +1,97 @@
+import numpy as np
+from astropy.io import fits
+from tools import get_path
+from scipy import interpolate
+
+def filt_para(filt):
+    para = {'v': {'fwhm': 769, 'zp': 17.89, 'cf': 2.61e-16},
+            'b': {'fwhm': 975, 'zp': 19.11, 'cf': 1.32e-16},
+            'u': {'fwhm': 785, 'zp': 18.34, 'cf': 1.5e-16},
+            'uw1': {'fwhm': 693, 'zp': 17.49, 'cf': 4.3e-16, 'rf': 0.138},
+            'um2': {'fwhm': 498, 'zp': 16.82, 'cf': 7.5e-16},
+            'uw2': {'fwhm': 657, 'zp': 17.35, 'cf': 6.0e-16}
+            }
+    return para[filt]
+
+def cr2mag(cr, cr_err, filt):
+    """use zero points to transfer counts to mag
+    """
+    zp = filt_para(filt)['zp']
+    mag = zp - 2.5*np.log10(cr)
+    if cr_err == False:
+        return mag
+    else:
+        mag_err = 2.5*cr_err/(np.log(10)*cr)
+        return mag, mag_err
+
+def cr2mean_flux(cr, cr_err, filt):
+    # read ea
+    ea_path = get_path('../data/auxil/arf_'+filt+'.fits')
+    ea_data = fits.open(ea_path)[1].data
+    ea_wave = (ea_data['WAVE_MIN']+ea_data['WAVE_MAX'])/20. # A to nm
+    ea_area = ea_data['SPECRESP']
+    delta_wave = ea_wave[1] - ea_wave[0]
+    factor = 0
+    for i in range(len(ea_wave)):
+        factor += ea_area[i]*ea_wave[i]*delta_wave*1e8*5.034116651114543
+    mean_flux = cr/(factor*10) # 10: nm to A
+    mean_flux_err = cr_err/(factor*10)
+    snr = mean_flux/mean_flux_err
+    return mean_flux, mean_flux_err, snr
+
+def cr2sb(cr, cr_err, filt, solid_angle):
+    """convert count rate to surface brightness
+    unit: W m-2 sr-1
+
+    cr to flux density: Poole et al. 2008
+    flux den to flux: flux den*FWHM
+    flux to sf: flux*factors/solid_angle
+    factors: 1 arcsec2 = 2.35e-11 sr
+             1 erg s-1 = 1e-7W
+             1 cm2 = 1e-4m2
+    """
+    fwhm = filt_para(filt)['fwhm']
+    cf = filt_para(filt)['cf']
+    factors = 1. #TODO:
+    sb = cr*cf*fwhm*factors/solid_angle
+    if cr_err == False: 
+        return sb
+    else:
+        sb_err = cr_err*cf*fwhm*factors/solid_angle
+        return sb, sb_err
+
+def mag_sb_flux_from_spec(spec_name, filt):
+    """use effective area and theoretical spectra
+    to calculate apparent magnitude
+    """
+    # read spectra
+    spec_path = get_path('../data/auxil/'+spec_name)
+    spec_wave = np.loadtxt(spec_path)[:, 0]
+    spec_flux = np.loadtxt(spec_path)[:, 1]*2.720E-4 # flux moment to irradiance
+    # read ea
+    ea_path = get_path('../data/auxil/arf_'+filt+'.fits')
+    ea_data = fits.open(ea_path)[1].data
+    ea_wave = (ea_data['WAVE_MIN']+ea_data['WAVE_MAX'])/20. # A to nm
+    ea_area = ea_data['SPECRESP']
+    # interpolate ea to cater for spec
+    ea = interpolate.interp1d(ea_wave, ea_area, fill_value='extrapolate')
+    spec_ea = ea(spec_wave)
+    wave_min = max([np.min(spec_wave), np.min(ea_wave)])
+    wave_max = min([np.max(spec_wave), np.max(ea_wave)])
+    spec = np.c_[np.c_[spec_wave, spec_flux.T], spec_ea.T]
+    spec_reduce = spec[spec[:,0]>wave_min, :]
+    spec_reduce = spec_reduce[spec_reduce[:,0]<wave_max, :]
+    spec = spec_reduce[spec_reduce[:,2]>0, :]
+    # integral
+    delta_wave = spec[1, 0] - spec[0, 0]
+    cr = 0
+    flux = 0
+    A = 0 # 30 
+    for i in range(len(spec)):
+        cr += spec[i, 0]*spec[i, 1]*spec[i, 2]*delta_wave*1e8*5.034116651114543
+        flux += spec[i, 1]*spec[i, 2]*delta_wave
+        A += spec[i, 2]*delta_wave
+    flux = flux/A
+    flux = flux/10. # nm to A
+    # cr to mag
+    return cr2mag(cr, False, filt), cr2sb(cr, False, filt, 1.), flux  
